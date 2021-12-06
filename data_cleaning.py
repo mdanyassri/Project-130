c16c5a17d365e992908a27fd76643f901453fba8
mdanyassri <72862261+mdanyassri@users.noreply.github.com>
2021-12-06 17:11:08 +0530

    Add files via upload

---
+++ data_cleaning.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import csv
+
+df = pd.read_csv("total_stars.csv")
+print(df.shape)
+
+print(df.columns)
+df.drop(['Unnamed: 0','Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1','Luminosity'],axis=1,inplace=True)
+print(df)
+
+final_data = df.dropna()
+print(final_data)
+
+final_data.reset_index(drop = True, inplace = True)
+print(final_data)
+
+final_data.to_csv("final.csv")
+final_data.info()
+final_data.describe()
+final_data.head(5)
+final_data.dtypes
